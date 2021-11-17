90ffdcd59bf653d36dcdf115a909d8ac582cd818
kkv <kirthikrishna30@gmail.com>
2021-11-17 13:04:34 +0530

    Added login and register component

---
+++ src/components/eLearningContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const eLearningContainer = () =>  {
+  return (
+    <div>
+      eLearningContainer component
+    </div>
+  )
+}
+
+export default eLearningContainer
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
 import App from './App';
+import configureStore from './store/configureStore'
 
-ReactDOM.render(<App />,document.getElementById('root'))
+const store = configureStore()
+console.log('state', store.getState());
+
+store.subcribe(() => {
+  console.log('state updated',store.getState());
+})
+
+ReactDOM.render(
+  <Provider store={store}><BrowserRouter>
+    <App />
+  </BrowserRouter></Provider>
+,document.getElementById('root'))
