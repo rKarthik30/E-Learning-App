43e863194535da7d52a4e18a125063c8b0d7ec84
kkv <kirthikrishna30@gmail.com>
2021-11-23 00:27:54 +0530

    action added

--- src/action/adminAction.js
+++ src/action/adminAction.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export const startAdminRegister = (adminData,reset) => {
     return (dispatch) => {
-        axios.post('https://dct-e-learning.herokuapp.com/api/admin/register',adminData)
+        axios.post('http://dct-e-learning.herokuapp.com/api/admin/register',adminData)
             .then((res) => {
                 const result = res.data
                 if(result.hasOwnProperty('errors')){
